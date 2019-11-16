b607b92d9f49a50209fc7fc2e0e471e97b3b0eb0
yuzuki <archc18yh27zno3k@gmail.com>
2019-11-16 12:12:06 +0900

    own.rs

--- ch5-1/danglingptr.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-
-int main()
-{
-    int *ptr = malloc(sizeof(int));
-    //メモリ領域を確保
-    free(ptr);
-    *ptr = 5;
-}
---
+++ ch5/ch5-2/own.rs
@@ -0,0 +1,13 @@
+fn main()
+{
+    func();
+}
+
+fn func()
+{
+    let s1 = String::from("Alice");
+    let s2 = String::from("Bob");
+
+    println!("s1 = {}", s1);
+    println!("s2 = {}", s2);
+}
